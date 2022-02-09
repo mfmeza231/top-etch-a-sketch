41be71f48608c70cafe48fe72ccc18b5ac31b20f
uniArchitect <hancho93@gmail.com>
2022-02-08 21:37:46 -0500

    created function for grid

--- top-etch-a-sketch.js
+++ top-etch-a-sketch.js
@@ -1,1 +1,18 @@
 //Global Scope
+const container = document.getElementById("container");
+
+//Create the Grid
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (c = 0; c < (rows * cols); c++) {
+        //creates a div element that will be a 'cell'
+        let cell = document.createElement('div');
+        //adds text within each cell created
+            //cell.innerText = (c+1);
+        container.appendChild(cell).className = "grid-item";
+    };
+};
+
+//Execute function to create grid
+makeGrid(16, 16);
